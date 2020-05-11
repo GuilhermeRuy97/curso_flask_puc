32ac5365440df7ca9f703aacd9a0f2ad86f9958c
walter-temporal <walter.temporal@gmail.com>
2020-05-11 15:18:26 -0300

    att 11/05/2020 - aula02

--- AULA02/app.py
+++ AULA02/app.py
@@ -1,36 +1,53 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, send_file
 from werkzeug.utils import secure_filename
 import threading
 import os
+from os import listdir
+from os.path import isfile, join
+
+mypath = '/home/walter/Documentos/curso_flask_puc/AULA02/downloads'
+username = 'Walter'
 
 app = Flask(__name__)
 
-
 @app.route("/", methods=['GET', 'POST'])
 def hello():
-    username = 'Walter'
-    #return render_template('upload_files.html', username=username)
+    
     if request.method == 'POST':
         if 'file' not in request.files:
             #return redirect(url_for('/'))
-            render_template('upload_files.html')
+            return render_template('upload_files.html', username=username)
 
         files = request.files.getlist('file')
 
         for file in files:
             filename = secure_filename(file.filename)
-            file.save(os.path.join('/home/walter/Documentos/curso_flask_puc/AULA02/downloads', filename))
+            file.save(os.path.join(mypath, filename))
 
-        #flash('Arquivo enviado com successo')
-        threadObj = threading.Thread(target=calcula(username))
-        threadObj.start()
+        
+        onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+        #threadObj = threading.Thread(target=calcula(username))
+        #threadObj.start()
         #return redirect('/relatorio')
-        return render_template('relatorio.html')
+        return render_template('relatorio.html', onlyfiles=onlyfiles, username=username)
 
-    return render_template('upload_files.html')
+    return render_template('upload_files.html', username=username)
 
-def calcula(username):
-    print('calculando...')
+@app.route('/download', methods=['GET', 'POST'])
+def downloadFile ():
+    if request.method == 'POST':
+        filename = request.form['filename']
+
+        onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+
+        if 'filename' not in request.form:
+            return render_template('upload_files.html', username=username, onlyfiles=onlyfiles)
+          
+        path = "downloads/"+filename
+        return send_file(path, as_attachment=True)
+    return render_template('download_file.html', username=username)
+
+def calcula(username):    
     pass
 
 if __name__ == "__main__":
