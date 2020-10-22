d4b37b4c2bf64af506805b73a0127e32c53a4659
walter-temporal <walter.temporal@gmail.com>
2020-10-22 18:50:49 -0300

    att

---
+++ aula_exemplo.py
@@ -0,0 +1,24 @@
+"""
+Esse codigo printa o hello world
+"""
+
+import numpy
+
+
+print("Hello world!")
+
+a = 5
+
+if a == 3:
+	print("Igual a 5")
+else:
+	print("Nao e 5")
+
+
+
+
+def printar_hello(a):
+	"""
+	Printa o hello world.
+	"""
+	print("Outro Hello.")
